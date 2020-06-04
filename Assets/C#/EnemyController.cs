15050e498ab86f93af21c314d303beef6988ba6e
Phoder1 <alon.talmi@gmail.com>
2020-06-04 19:41:09 +0300

    changes

--- Assets/C#/EnemyController.cs
+++ Assets/C#/EnemyController.cs
@@ -2,23 +2,24 @@
 
 public class EnemyController : MonoBehaviour {
     float collisionForce;
-    Rigidbody2D rb;
+    Rigidbody rb;
     const float forceAmount = 50f;
     float impulseForce;
     [SerializeField]
     float maxImpulseForce = 1.5f;
     private void Start() {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody>();
     }
     // Update is called once per frame
     void Update() {
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter(Collision collision) {
         collisionForce = PlayerController.rotationAngle;
-        impulseForce = Mathf.Clamp(Mathf.Abs(collisionForce) * Time.deltaTime * forceAmount,0f, maxImpulseForce);
-        rb.AddForce(((Vector2)transform.position - collision.GetContact(0).point) * impulseForce, ForceMode2D.Impulse);
+        impulseForce = Mathf.Clamp(Mathf.Abs(collisionForce) * Time.deltaTime * forceAmount, 0f, maxImpulseForce);
+        rb.AddForce((transform.position - collision.GetContact(0).point) * impulseForce, ForceMode.Impulse);
         Debug.Log("NITROOOOOO : " + impulseForce);
     }
+
 }
