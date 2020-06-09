f9f3fb084b75c44fdb24485eb2db9f511510294e
Phoder1 <alon.talmi@gmail.com>
2020-06-09 16:28:43 +0300

    Camera controller

--- Assets/C#/Camera.cs
+++ Assets/C#/Camera.cs
@@ -1,32 +1,35 @@
 ﻿using UnityEngine;
 public class Camera : MonoBehaviour
 {
-    float CameraMoveY;
     public float cameraRotSpeed;
     public Transform playerTrans;
 
     public Transform camTrans;
 
-    public float xRotOffSet;
-    public float zDistanceFromPlayer;
-    public float camHeight;
+    public float rotFromPlayer;
+    public float distanceFromPlayer;
+    public float cameraRotOffset;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        camTrans.transform.localRotation = Quaternion.Euler(xRotOffSet, 0f, 0f);
-        camTrans.transform.localPosition = new Vector3(0f, camHeight, zDistanceFromPlayer);
+        camTrans.transform.localPosition = (Quaternion.Euler(rotFromPlayer, 0f, 0f) * Vector3.back) * distanceFromPlayer;
+
+        camTrans.rotation = Quaternion.LookRotation(playerTrans.position - camTrans.position, Vector3.up) * Quaternion.Euler(cameraRotOffset,0f,0f);
+
+        //camTrans.transform.localRotation = Quaternion.Euler(xRotOffSet, 0f, 0f);
+        //camTrans.transform.localPosition = new Vector3(0f, camHeight, zDistanceFromPlayer);
         transform.position = playerTrans.position;
-        CameraMoveY = Input.GetAxis("Mouse X");
         if (Input.GetMouseButton(2))
         {
-            transform.Rotate(0f, CameraMoveY * cameraRotSpeed, 0f);
+            transform.Rotate(0f, Input.GetAxis("Mouse X") * cameraRotSpeed * Time.deltaTime, 0f);
         }
 
     }
